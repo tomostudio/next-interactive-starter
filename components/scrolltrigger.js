8f1a6afe50a0adf7d6ebbf9ae816cadb657de53f
hendhyhutomo <hendhyhutomo@gmail.com>
2023-02-09 20:12:47 +0700

    Update ScrollTrigger

--- components/scrolltrigger.js
+++ components/scrolltrigger.js
@@ -56,156 +56,134 @@ const ScrollTriggerWrapper = forwardRef((props, ref) => {
 
   // init timeilne / animation
   useEffect(() => {
-    let tlSaveStyle = [];
-    let currentTL = {};
+    let ctx = gsap.context(() => {
+      let tlSaveStyle = [];
+      let currentTL = {};
 
-    const resetAnimation = () => {
-      //Get All Timeline and Clear
-      gsap.globalTimeline.clear();
 
-      if (currentTL) {
-        Object.entries(currentTL).forEach(([key, tl]) => {
-          // go through each timeline.
-          tl.forEach((eachTL) => {
-            // kill timeline
-            eachTL.pause(0).kill(true);
-          });
-          // delete cleared object
-          delete currentTL[key];
-        });
-      }
+      const applyAnimation = ({ anim, tl = [], ss }) => {
+        // Anim = animation
+        // tl = timeline to push
+        // ss = save style
+        if (anim) {
+          // check if there are multiple animation
+          if (anim instanceof Array) {
+            // run multiple animation command
+            anim.forEach((each_anim) => {
+              // push each animation into array.
+              // pushing animation = running the animation.
+              const { settings, elem, animation } = each_anim();
 
-      //Get All Scroll Trigger and Clear
-      let allTrigger = ScrollTrigger.getAll();
-      allTrigger.forEach((trigger) => {
-        trigger.kill(true);
-      });
+              tl.push(gsap.timeline(settings));
 
-      //Clear Array
-      tlSaveStyle = [];
-    };
+              if (ss) ss.push(elem);
 
-    const applyAnimation = ({ anim, tl = [], ss }) => {
-      // Anim = animation
-      // tl = timeline to push
-      // ss = save style
-      if (anim) {
-        // check if there are multiple animation
-        if (anim instanceof Array) {
-          // run multiple animation command
-          anim.forEach((each_anim) => {
-            // push each animation into array.
-            // pushing animation = running the animation.
-            const { settings, elem, animation } = each_anim();
-
-            tl.push(gsap.timeline(settings));
-
-            if (ss) ss.push(elem);
-
-            animation.forEach((a) => {
-              const k = Object.keys(a)[0];
-              if (k === 'to') {
-                tl[tl.length - 1].to(...a[k]);
-              } else if (k === 'from') {
-                tl[tl.length - 1].from(...a[k]);
-              } else if (k === 'set') {
-                tl[tl.length - 1].set(...a[k]);
-              } else if (k === 'call') {
-                tl[tl.length - 1].call(a[k]);
-              }
+              animation.forEach((a) => {
+                const k = Object.keys(a)[0];
+                if (k === 'to') {
+                  tl[tl.length - 1].to(...a[k]);
+                } else if (k === 'from') {
+                  tl[tl.length - 1].from(...a[k]);
+                } else if (k === 'set') {
+                  tl[tl.length - 1].set(...a[k]);
+                } else if (k === 'call') {
+                  tl[tl.length - 1].call(a[k]);
+                }
+              });
             });
+          }
+
+          return tl;
+        } else if (isFunction(anim)) {
+          // pull object
+          const { settings, elem, animation } = anim();
+
+          // push to array
+          tl.push(gsap.timeline(settings));
+
+          if (ss) ss.push(elem);
+
+          animation.forEach((a) => {
+            const k = Object.keys(a)[0];
+            if (k === 'to') {
+              tl[tl.length - 1].to(...a[k]);
+            } else if (k === 'from') {
+              tl[tl.length - 1].from(...a[k]);
+            }
           });
         }
+      };
 
-        return tl;
-      } else if (isFunction(anim)) {
-        // pull object
-        const { settings, elem, animation } = anim();
+      if (scrollInitState && animation) {
+        if (animation instanceof Object && !(animation instanceof Array)) {
+          const _property = Object.getOwnPropertyNames(animation);
 
-        // push to array
-        tl.push(gsap.timeline(settings));
+          //Create Array for Match Media
+          const stMatchMedia = [];
 
-        if (ss) ss.push(elem);
+          // fill animation
+          _property.forEach((p, id) => {
+            // push animation to object
 
-        animation.forEach((a) => {
-          const k = Object.keys(a)[0];
-          if (k === 'to') {
-            tl[tl.length - 1].to(...a[k]);
-          } else if (k === 'from') {
-            tl[tl.length - 1].from(...a[k]);
-          }
-        });
-      }
-    };
+            const pushData = {
+              media: p,
+              function: function () {
+                //run apply animation function
 
-    if (scrollInitState && animation) {
-      if (animation instanceof Object && !(animation instanceof Array)) {
-        const _property = Object.getOwnPropertyNames(animation);
-
-        //Create Array for Match Media
-        const stMatchMedia = [];
-
-        // fill animation
-        _property.forEach((p, id) => {
-          // push animation to object
-
-          const pushData = {
-            media: p,
-            function: function () {
-              //run apply animation function
-
-              currentTL[`${p}`] = applyAnimation({
-                anim: animation[p],
-                ss: tlSaveStyle,
-              });
-
-              return () => {
-                tlSaveStyle.forEach((ss) => {
-                  gsap.set(ss, { clearProps: true });
+                currentTL[`${p}`] = applyAnimation({
+                  anim: animation[p],
+                  ss: tlSaveStyle,
                 });
 
-                ScrollTrigger.refresh(true);
-                //Kill All Timeline during breakpoint
-                if (currentTL[`${p}`])
-                  currentTL[`${p}`].forEach((eachTL) => {
-                    eachTL.pause(0).kill();
-                    eachTL.clear();
+                return () => {
+                  tlSaveStyle.forEach((ss) => {
+                    gsap.set(ss, { clearProps: true });
                   });
 
-                delete currentTL[`${p}`];
-              };
-            },
-          };
-          stMatchMedia.push(pushData);
-        });
+                  ScrollTrigger.refresh(true);
+                  //Kill All Timeline during breakpoint
+                  if (currentTL[`${p}`])
+                    currentTL[`${p}`].forEach((eachTL) => {
+                      eachTL.pause(0).kill();
+                      eachTL.clear();
+                    });
 
-        // RUN Scrolltrigger MatchMedia
-        // ScrollTrigger.matchMedia(stMatchMedia);
+                  delete currentTL[`${p}`];
+                };
+              },
+            };
+            stMatchMedia.push(pushData);
+          });
 
-        let mm = gsap.matchMedia();
+          // RUN Scrolltrigger MatchMedia
+          // ScrollTrigger.matchMedia(stMatchMedia);
 
-        stMatchMedia.forEach((mediaQuery) => {
-          mm.add(mediaQuery.media, mediaQuery.function);
-        });
+          let mm = gsap.matchMedia();
 
-        // Set ScrollTrigger Save Styles
-        let saveStyles = ``;
-        tlSaveStyle.forEach((tl) => {
-          saveStyles += `${tl} ,`;
-        });
-        if (saveStyles.slice(-2) === ' ,') {
-          saveStyles = saveStyles.slice(0, -2);
+          stMatchMedia.forEach((mediaQuery) => {
+            mm.add(mediaQuery.media, mediaQuery.function);
+          });
+
+          // Set ScrollTrigger Save Styles
+          let saveStyles = ``;
+          tlSaveStyle.forEach((tl) => {
+            saveStyles += `${tl} ,`;
+          });
+          if (saveStyles.slice(-2) === ' ,') {
+            saveStyles = saveStyles.slice(0, -2);
+          }
+
+          ScrollTrigger.saveStyles(saveStyles);
+        } else {
+          // Fill Animation normally, no breakpoints
+          resetAnimation();
+          currentTL[`global`] = applyAnimation({ anim: animation });
         }
-
-        ScrollTrigger.saveStyles(saveStyles);
-      } else {
-        // Fill Animation normally, no breakpoints
-        resetAnimation();
-        currentTL[`global`] = applyAnimation({ anim: animation });
       }
-    }
+    });
     return () => {
-      resetAnimation();
+      ctx.revert();
+      // resetAnimation();
     };
   }, [scrollInitState, animation]);
 
