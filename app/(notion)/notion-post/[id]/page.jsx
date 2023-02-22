42538e31518e05230307dd66378611ec92e43956
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-02-22 17:20:24 +0700

    update api

--- app/(notion)/notion-post/[id]/page.jsx
+++ app/(notion)/notion-post/[id]/page.jsx
@@ -4,23 +4,23 @@ import FancyLink from '@/components/fancyLink'
 import Layout from '@/components/layout'
 import Image from 'next/image'
 import { use } from 'react'
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from "react-markdown";
 
-// const getData = async (id) =>
-//   fetch(`http://localhost:3000/api/notion/getDetailPost/${id}`).then((res) =>
-//     res.json(),
-//   )
+const getData = async (id) =>
+  fetch(`https://next-interactive-starter.vercel.app/api/notion/getDetailPost/${id}`).then((res) =>
+    res.json(),
+  )
 
 const PostPage = ({ params }) => {
   const { id } = params
 
-  // const post = use(getData(id))
+  const post = use(getData(id))
 
-  // if (post.status === 404) return null
+  if (post.status === 404) return null
 
   return (
     <Layout>
-      {/* <div className="max-w-screen-lg w-full mx-auto flex flex-col space-y-8 px-4 mt-10">
+      <div className="max-w-screen-lg w-full mx-auto flex flex-col space-y-8 px-4 mt-10">
         <FancyLink destination="/sanity-post" label="Back" />
         <h1 className="text-4xl">
           {post.page.properties.Name.title[0].plain_text}
@@ -40,7 +40,7 @@ const PostPage = ({ params }) => {
             <ReactMarkdown>{post.blocks}</ReactMarkdown>
           </div>
         </div>
-      </div> */}
+      </div>
     </Layout>
   )
 }
