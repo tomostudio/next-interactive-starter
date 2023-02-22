60258db44d3c3f77d328228161a092069a2242a7
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-02-22 15:20:13 +0700

    update api

--- app/(notion)/notion-post/page.jsx
+++ app/(notion)/notion-post/page.jsx
@@ -12,6 +12,36 @@ const NotionPost = () => {
     <Layout>
       <div className="max-w-screen-lg w-full mx-auto flex flex-col space-y-8 px-4 mt-10">
         <h1 className="text-4xl">Notion Post</h1>
+        {post?.map(({properties, cover}, id) => (
+          <div
+            className="w-full h-fit flex flex-col space-y-2 border-2 rounded border-black bg-gray-100"
+            key={id}
+          >
+            <div className="relative w-full h-48">
+              <Image
+                className="p-2"
+                src={cover.file.url}
+                alt={properties.Name.title[0].plain_text}
+                fill
+                style={{
+                  objectFit: 'contain',
+                }}
+              />
+            </div>
+            <h2 className="text-2xl px-4 py-2 border-t-2 border-black">
+              {properties.Name.title[0].plain_text}
+            </h2>
+            <div className="px-4 py-2">
+              <div className="mt-6">
+                <FancyLink
+                  destination={`/sanity-post/${properties.Slug.formula.string}`}
+                  a11yText="Navigate to the about page"
+                  label="READ MORE"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </Layout>
   )
