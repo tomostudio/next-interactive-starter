8ed329a56be7b59e6f74a66803095cef599bab4f
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-02-22 15:18:38 +0700

    update api

--- app/(notion)/notion-post/page.jsx
+++ app/(notion)/notion-post/page.jsx
@@ -5,9 +5,9 @@ import Layout from '@/components/layout'
 import Image from 'next/image'
 import { use, useEffect, useState } from 'react'
 
-// const getData = async () => fetch('https://next-interactive-starter.vercel.app/api/notion').then((res) => res.json())
+const getData = async () => fetch('https://next-interactive-starter.vercel.app/api/notion').then((res) => res.json())
 const NotionPost = () => {
-//   const post = use(getData())
+  const post = use(getData())
   return (
     <Layout>
       <div className="max-w-screen-lg w-full mx-auto flex flex-col space-y-8 px-4 mt-10">
