446df7fc5c9ce1e3d14ac1159870a0ded14d1c35
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-07-24 14:13:18 +0700

    💄 add video mobile

---
+++ app/video-mobile/page.jsx
@@ -0,0 +1,17 @@
+// import sample from '@/public/assets/sample.mp4'
+import sample from '@/public/assets/image.png'
+
+const Page = () => {
+  return (
+    <div className="w-full h-[200vh] flex flex-col">
+      <div>
+        <video autoPlay muted playsInline>
+          <source src="/assets/sample.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  )
+}
+
+export default Page
