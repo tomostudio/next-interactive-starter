63a8fa3b969fef7d765b3b2e176329af3996c44c
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-07-24 14:17:57 +0700

    💄 update video loop

--- app/video-mobile/page.jsx
+++ app/video-mobile/page.jsx
@@ -5,7 +5,7 @@ const Page = () => {
   return (
     <div className="w-full h-[200vh] flex flex-col">
       <div>
-        <video autoPlay muted playsInline>
+        <video autoPlay muted playsInline loop>
           <source src="/assets/sample.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
